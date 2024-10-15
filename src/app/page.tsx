f758db47cf59e4a04818d4ecc0ca483740aa23cf
gabriel <andreyalth@gmail.com>
2024-10-15 14:49:34 -0600

    agregar iconos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Header } from './components/Header'
 import { ImageContainer } from './components/ImageContainer'
+import { Services } from './partial/services'
 export default function Home() {
   return (
     <div>
       <Header/>
       <ImageContainer/>
+      <Services/>
     </div>
   );
 }

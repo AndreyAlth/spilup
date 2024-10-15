1f3c144ff7568bdd7ed22b2efbd5c0cf525bd115
gabriel <andreyalth@gmail.com>
2024-10-15 14:51:02 -0600

    agregar seccion de servicios

--- src/app/partial/services.tsx
+++ src/app/partial/services.tsx
@@ -1,12 +1,42 @@
 import React from 'react'
-import { AngleIcon, CheckIcon } from '@radix-ui/react-icons'
 import { Check } from 'lucide-react'
 
 export const Services = () => {
   return (
     <div className='grid grid-cols-6 gap-5 bg-pink-500 m-4'>
         <div className='flex flex-col justify-center items-center'>
-            <CheckIcon/>
+            <Check/>
+            <p className='text-center'>Reconocimiento facial, identificación de cara, incluso con gafas</p>
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+            <Check/>
+            <p className='text-center'>Reconocimiento facial, identificación de cara, incluso con gafas</p>
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+            <Check/>
+            <p className='text-center'>Reconocimiento facial, identificación de cara, incluso con gafas</p>
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+            <Check/>
+            <p className='text-center'>Reconocimiento facial, identificación de cara, incluso con gafas</p>
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+            <Check/>
+            <p className='text-center'>Reconocimiento facial, identificación de cara, incluso con gafas</p>
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+            <Check/>
+            <p className='text-center'>Reconocimiento facial, identificación de cara, incluso con gafas</p>
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+            <Check/>
+            <p className='text-center'>Reconocimiento facial, identificación de cara, incluso con gafas</p>
+        </div>
+        <div className='flex flex-col justify-center items-center'>
+            <Check/>
+            <p className='text-center'>Reconocimiento facial, identificación de cara, incluso con gafas</p>
+        </div>
+        <div className='flex flex-col justify-center items-center'>
             <Check/>
             <p className='text-center'>Reconocimiento facial, identificación de cara, incluso con gafas</p>
         </div>

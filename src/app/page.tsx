d1717939d6c390d097b70d68f143ca468b5f88a3
gabriel <andreyalth@gmail.com>
2025-02-20 21:07:00 -0600

    agregar elementos de diseño

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,23 @@ import { Services } from "./partial/Services";
 import { Contact } from "./partial/Contacto";
 import { Footer } from './components/Footer'
 import { Provider } from './i18n/Provider'
+import { PageLayout } from './new-layout/PageLayout'
+import { LandingHeader } from '@/app/components/quland/LandingHeader'
+
 export default function Home() {
   return (
     <div>
-      <Provider/>
+      <PageLayout>
+        <div className="home-four relative bg-[#0a0118] w-full overflow-x-hidden">
+          <LandingHeader />
+        </div>
+      </PageLayout>
+      {/* <Provider/>
       <Header />
       <Hero />
       <Services />
       <Contact />
-      <Footer/>
+      <Footer/> */}
     </div>
   );
 }

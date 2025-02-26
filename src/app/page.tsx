193fa0631152d7a4dd1baf712e0c2fc2004c1c8f
gabriel <andreyalth@gmail.com>
2025-02-25 20:42:46 -0600

    agregar aos compenete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import AOS from 'aos'; 
 import { PageLayout } from './new-layout/PageLayout'
 import { LandingHeader } from '@/app/components/quland/LandingHeader'
 import { Hero } from '@/app/components/quland/Hero'
@@ -7,12 +9,18 @@ import { TestimonialSection } from 'components/quland/TestimonialSection'
 import { FaqSection } from 'components/quland/FaqSection'
 import { Prices} from 'components/quland/Prices'
 import { Services } from 'components/quland/Services'
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+    console.log(AOS)
+  }, []);
   return (
     <div>
       <PageLayout>
-        <div className="home-four relative bg-[#0a0118] w-full overflow-x-hidden">
+        <div className="home---save-dev @types/aosfour relative bg-[#0a0118] w-full overflow-x-hidden">
           <LandingHeader />
           <Hero />
           <FeatureSection />

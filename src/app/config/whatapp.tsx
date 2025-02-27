e170656ba083bfd466e03880925a28283708eab1
gabriel <andreyalth@gmail.com>
2025-02-27 17:16:35 -0600

    corregir numero de whats

--- src/app/config/whatapp.tsx
+++ src/app/config/whatapp.tsx
@@ -1,4 +1,4 @@
 export const message = {
-    "contact": "https://wa.me/52999584115?text=Hola,%20me%20gustar%C3%ADa%20informaci%C3%B3n%20sobre%20una%20aplicaci%C3%B3n",
-    "bots": "https://wa.me/52999584115?text=Hola,%20me%20gustar%C3%ADa%20informaci%C3%B3n%20sobre%20un%20Bot%20o%20agente%20AI"
+    "contact": "https://wa.me/529993584115?text=Hola,%20me%20gustar%C3%ADa%20informaci%C3%B3n%20sobre%20una%20aplicaci%C3%B3n",
+    "bots": "https://wa.me/529993584115?text=Hola,%20me%20gustar%C3%ADa%20informaci%C3%B3n%20sobre%20un%20Bot%20o%20agente%20AI"
 }

cea2e36bf253883caf137c0cdfd27e0e7c1c8a9c
gabriel <andreyalth@gmail.com>
2025-02-27 15:56:10 -0600

    agregar idioma en navbar mobil

--- src/app/components/quland/MobileHeader.tsx
+++ src/app/components/quland/MobileHeader.tsx
@@ -1,10 +1,11 @@
 'use client';
 import React, { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
+import { useTranslation } from 'react-i18next';
 
 const menuData = [
   {
-    title: 'Home',
+    title: 'navbar.services',
     subMenu: [
       { title: 'Digital Marketing', href: '' },
       { title: 'SEO Agency', href: '/home-two' },
@@ -18,26 +19,14 @@ const menuData = [
       { title: 'Personal Portfolio', href: '/home-ten' }
     ]
   },
-  { title: 'Company', href: '/about' },
-  { title: 'Services', href: '/services' },
-  { title: 'Blogs', href: '/blogs' },
+  { title: 'FAQ', href: '/about' },
   {
-    title: 'Pages',
+    title: 'navbar.lenguage',
     subMenu: [
-      { title: 'Projects', href: '/projects' },
-      { title: 'Projects Details', href: '/project-details/' },
-      { title: 'Service Details', href: '/service-details' },
-      { title: 'Blog Details', href: '/blog-details' },
-      { title: 'Team Members', href: '/teams' },
-      { title: "FAQ's", href: '/faqs' },
-      { title: 'Pricing', href: '/pricing' },
-      { title: 'Course Details', href: '/course-details' },
-      { title: 'Cart', href: '/cart' },
-      { title: 'Checkout', href: '/checkout' },
-      { title: 'Not Found', href: '/notfound' }
+      { title: 'Es', button: true, value: 'es' },
+      { title: 'En', button: true, value: 'en' }
     ]
-  },
-  { title: 'Contact', href: 'contact' }
+  }
 ];
 
 export const MobileHeader = () => {
@@ -45,6 +34,11 @@ export const MobileHeader = () => {
   const [activeSubNav, setActiveSubNav] = useState(null);
   const navRef = useRef(null);
   const overlayRef = useRef<HTMLDivElement>(null);
+  const { i18n, t } = useTranslation()
+  
+    const changeLen = (len:string) => {
+      i18n.changeLanguage(len)
+    }
 
   const toggleNav = () => {
     setIsNavActive(prev => !prev);
@@ -146,7 +140,7 @@ export const MobileHeader = () => {
                         className="relative flex items-center gap-2 leading-5 home-two-nav-item w-fit m-nav-dropdown"
                         onClick={() => toggleSubNav(index)}
                       >
-                        {menuItem.title}
+                        {t(menuItem.title)}
                         <svg
                           className="pointer-events-none"
                           width="10"
@@ -165,18 +159,27 @@ export const MobileHeader = () => {
                         </svg>
                       </button>
                       <ul
-                        className={`overflow-hidden transition-all font-medium h-[150px] duration-300 pl-5 pt-4 ${
+                        className={`overflow-hidden transition-all font-medium h-fit duration-300 pl-5 pt-4 ${
                           activeSubNav === index ? '' : 'mobile-sub-nav'
                         }`}
                       >
                         {menuItem.subMenu.map((subItem, subIndex) => (
                           <li key={subIndex} className="relative py-1">
-                            <a
-                              className="relative py-1 font-medium leading-5 home-two-nav-item hover:text-purple w-fit"
-                              href={subItem.href}
-                            >
-                              {subItem.title}
-                            </a>
+                            {subItem.button === true ? (
+                              <button
+                                className="relative home-two-nav-item w-fit"
+                                onClick={() =>  changeLen(subItem.value)}
+                              >
+                                {subItem.title}
+                              </button>
+                            ) : (
+                              <a
+                                className="relative home-two-nav-item w-fit"
+                                href={subItem.href}
+                              >
+                                {subItem.title}
+                              </a>
+                            )}
                           </li>
                         ))}
                       </ul>
@@ -194,36 +197,10 @@ export const MobileHeader = () => {
             </ul>
           </div>
           <div className="flex flex-col gap-5 pl-5 mt-5">
-            <a href="#" className="flex gap-2.5 group">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M3.77762 11.9424C2.8296 10.2893 2.37185 8.93948 2.09584 7.57121C1.68762 5.54758 2.62181 3.57081 4.16938 2.30947C4.82345 1.77638 5.57323 1.95852 5.96 2.6524L6.83318 4.21891C7.52529 5.46057 7.87134 6.08139 7.8027 6.73959C7.73407 7.39779 7.26737 7.93386 6.33397 9.00601L3.77762 11.9424ZM3.77762 11.9424C5.69651 15.2883 8.70784 18.3013 12.0576 20.2224M12.0576 20.2224C13.7107 21.1704 15.0605 21.6282 16.4288 21.9042C18.4524 22.3124 20.4292 21.3782 21.6905 19.8306C22.2236 19.1766 22.0415 18.4268 21.3476 18.04L19.7811 17.1668C18.5394 16.4747 17.9186 16.1287 17.2604 16.1973C16.6022 16.2659 16.0661 16.7326 14.994 17.666L12.0576 20.2224Z"
-                  stroke="#5D51F2"
-                  strokeWidth="1.5"
-                  strokeLinejoin="round"
-                />
-                <path
-                  d="M22 5L12 5M22 5C22 4.43982 20.604 3.39322 20.25 3M22 5C22 5.56018 20.604 6.60678 20.25 7M12 5C12 4.43982 13.396 3.39322 13.75 3M12 5C12 5.56018 13.396 6.60678 13.75 7"
-                  stroke="#5D51F2"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              <span className="text-base font-bold leading-5 tracking-tight text-main-black">
-                Call : 000 (123) 456 88
-              </span>
-            </a>
             <Link href="/contact">
               <div className="home-two-btn-bg py-2.5 group bg-blue-seo border-blue-seo w-fit">
                 <span className="relative z-10 text-base font-semibold text-white transition-all duration-300 group-hover:text-blue-seo font-inter">
-                  Contact US
+                  {t('navbar.contact')}
                 </span>
                 <svg
                   className="relative z-10"

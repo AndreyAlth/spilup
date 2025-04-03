8a9ba0f6c35c5aab5cee79a70c5aefcbcbdeef5f
gabriel <andreyalth@gmail.com>
2025-04-03 17:27:19 -0600

    eliminar productos

--- src/app/config/services.tsx
+++ src/app/config/services.tsx
@@ -1,4 +1,11 @@
-import { AppWindow, AudioLines, BotMessageSquare, Captions, Workflow } from "lucide-react";
+/* eslint-disable no-unused-vars */
+import {
+  AppWindow,
+  // AudioLines,
+  BotMessageSquare,
+  // Captions,
+  Workflow
+} from 'lucide-react';
 import { Service } from "../types/Services";
 
 export const services: Service[] = [
@@ -76,66 +83,66 @@ export const services: Service[] = [
       }
     ]
   },
-  {
-    id: 'transcripcion',
-    icon: <Captions size={45} stroke='white' strokeWidth={1}/>,
-    title: 'transcripcion.title',
-    subtitle: 'transcripcion.subtitle',
-    img_url: null,
-    video_text: 'Ver demo',
-    video_url: 'https://www.youtube.com/watch?v=0O2aH4XLbto',
-    btn_text: 'Más información',
-    btn_link: '#',
-    features: {
-      img: '/assets/images/home-four/features-thumb.webp',
-      title: 'Automatización de procesos',
-      list: [
-        {
-          id: 1,
-          title: 'Automatización de procesos',
-          description:
-            'Automatiza los procesos de tu agencia y ahorra tiempo y dinero.'
-        }
-      ]
-    },
-    faqs: [
-      {
-        id: 1,
-        question: '¿Qué es un CRM?',
-        answer:
-          'Un CRM es un software que te permite gestionar las relaciones con tus clientes.'
-      }
-    ],
-  },
-  {
-    id: 'voiceprint',
-    icon: <AudioLines size={45} stroke='white' strokeWidth={1}/>,
-    title: 'voiceprint.title',
-    subtitle: 'voiceprint.subtitle',
-    img_url: null,
-    video_text: 'Ver demo',
-    video_url: 'https://www.youtube.com/watch?v=0O2aH4XLbto',
-    btn_text: 'Más información',
-    btn_link: '#',
-    features: {
-      img: '/assets/images/home-four/features-thumb.webp',
-      title: 'Automatización de procesos',
-      list: [
-        {
-          id: 1,
-          title: 'Automatización de procesos',
-          description:  'Automatiza los procesos de tu agencia y ahorra tiempo y dinero.'
-        }
-      ],
-    },
-    faqs: [
-      {
-        id: 1,
-        question: '¿Qué es un CRM?',
-        answer: 'Un CRM es un software que te permite gestionar las relaciones con tus clientes.',
-      }
-    ],
-  },
+  // {
+  //   id: 'transcripcion',
+  //   icon: <Captions size={45} stroke='white' strokeWidth={1}/>,
+  //   title: 'transcripcion.title',
+  //   subtitle: 'transcripcion.subtitle',
+  //   img_url: null,
+  //   video_text: 'Ver demo',
+  //   video_url: 'https://www.youtube.com/watch?v=0O2aH4XLbto',
+  //   btn_text: 'Más información',
+  //   btn_link: '#',
+  //   features: {
+  //     img: '/assets/images/home-four/features-thumb.webp',
+  //     title: 'Automatización de procesos',
+  //     list: [
+  //       {
+  //         id: 1,
+  //         title: 'Automatización de procesos',
+  //         description:
+  //           'Automatiza los procesos de tu agencia y ahorra tiempo y dinero.'
+  //       }
+  //     ]
+  //   },
+  //   faqs: [
+  //     {
+  //       id: 1,
+  //       question: '¿Qué es un CRM?',
+  //       answer:
+  //         'Un CRM es un software que te permite gestionar las relaciones con tus clientes.'
+  //     }
+  //   ],
+  // },
+  // {
+  //   id: 'voiceprint',
+  //   icon: <AudioLines size={45} stroke='white' strokeWidth={1}/>,
+  //   title: 'voiceprint.title',
+  //   subtitle: 'voiceprint.subtitle',
+  //   img_url: null,
+  //   video_text: 'Ver demo',
+  //   video_url: 'https://www.youtube.com/watch?v=0O2aH4XLbto',
+  //   btn_text: 'Más información',
+  //   btn_link: '#',
+  //   features: {
+  //     img: '/assets/images/home-four/features-thumb.webp',
+  //     title: 'Automatización de procesos',
+  //     list: [
+  //       {
+  //         id: 1,
+  //         title: 'Automatización de procesos',
+  //         description:  'Automatiza los procesos de tu agencia y ahorra tiempo y dinero.'
+  //       }
+  //     ],
+  //   },
+  //   faqs: [
+  //     {
+  //       id: 1,
+  //       question: '¿Qué es un CRM?',
+  //       answer: 'Un CRM es un software que te permite gestionar las relaciones con tus clientes.',
+  //     }
+  //   ],
+  // },
   {
     id: 'automatization',
     icon: <Workflow size={45} stroke='white' strokeWidth={1}/>,
